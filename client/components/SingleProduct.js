2abd533595ac8cdd5bedd1374e12e8fa7ef345cf
Anh Duong <anhhduong1609@gmail.com>
2022-03-01 16:45:50 -0500

    Backend added

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchSingleProduct } from '../store/singleProduct';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchSingleProduct } from "../store/singleProduct";
 
 class SingleProduct extends React.Component {
   componentDidMount() {
     this.props.loadSingleProduct(this.props.match.params.id);
   }
   render() {
-      const product = this.props.product
+    const product = this.props.product;
     return (
-        <div>
-            <img src={product.imageUrl}></img>
-            <h1>{product.name}</h1>
-            <h2>{product.description}</h2>
-            <h3>${product.price}</h3>
-        </div>
-    )
+      <div>
+        <img src={product.imageUrl}></img>
+        <h1>{product.name}</h1>
+        <h2>{product.description}</h2>
+        <h3>${product.price}</h3>
+      </div>
+    );
   }
 }
 
--- server/api/products.js
+++ server/api/products.js
@@ -12,3 +12,32 @@ router.get("/:id", async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).send(newProduct);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const productToBeDeleted = await Product.findByPk(req.params.id);
+    await productToBeDeleted.destroy();
+    res.send(productToBeDeleted);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.update(req.body);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
