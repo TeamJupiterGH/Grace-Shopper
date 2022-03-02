fee027078669802aeeb59b005a6c279d51091fdf
EB <sheunbee@gmail.com>
2022-03-02 16:07:02 -0500

    edited form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -11,6 +11,24 @@ const AuthForm = props => {
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
+      <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
         <div>
           <label htmlFor="username">
             <small>Username</small>
