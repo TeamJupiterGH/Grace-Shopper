033017118cda8a13b17a3a27f4b838dd597057fd
EB <sheunbee@gmail.com>
2022-03-02 20:21:07 -0500

    added user routes

--- server/api/users.js
+++ server/api/users.js
@@ -11,9 +11,50 @@ router.get("/", async (req, res, next) => {
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ["id", "username"],
-    });
+    }
+    );
     res.json(users);
   } catch (err) {
     next(err);
   }
 });
+
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).send(newUser);
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try{
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async(req, res, next) => {
+  try{
+    const userToBeDeleted= await User.findByPk(req.params.id);
+    await userToBeDeleted.destroy();
+    res.send(userToBeDeleted);
+  } catch(error) {
+    next(error);
+  }
+})
+
+router.put("/:id", async(req, res, next) => {
+  try{
+    const user = await User.findByPk(req.params.id);
+    await user.update(req.body);
+    res.json(product);
+  } catch(error) {
+    next(error);
+  }
+})
+
