9bdd95b467c4e20c3fef3fb978db2cfe3c04df24
EB <sheunbee@gmail.com>
2022-03-08 17:09:24 -0500

    modified api guests

--- server/api/guests.js
+++ server/api/guests.js
@@ -4,17 +4,7 @@ const {
 } = require("../db");
 module.exports = router;
 
-// router.get("/:guestId/cart", async (req, res, next) => {
-//     const guestId = req.params.guestId;
-//     try {
-//         const order = await Order.findOne({
-//             where
-//         })
-//     }
-// })
-
 router.post("/guest", async (req, res, next) => {
-  //req.body = {firstName: "EB", lastName: "Hong", email: "EB@gmail.com", tempCart:[{id: 1, quantity: 3}, {id: 4, quantity: 6}]}
   try {
     const guest = await User.create({
       firstName: req.body.firstName,
@@ -22,17 +12,25 @@ router.post("/guest", async (req, res, next) => {
       email: req.body.email,
     });
 
-    // const tempCart = JSON.str(req.body.tempCart);
     const tempCart = req.body.tempCart;
     const order = await Order.create({ userId: guest.id });
-    const order_details = await Order_Details.create({
-      orderId: order.id,
-      productId: tempCart[0].id,
-      quantity: tempCart[0].quanity,
-    });
 
-    res.send(order_details);
-    // res.send(guest);
+    for (let i = 0; i < tempCart.length; i++) {
+        let current = tempCart[i];
+
+        await Order_Details.create({
+            orderId: order.id,
+            productId: current.id,
+            quantity: current.quantity
+        })
+    }
+    const orderDetails = await Order.findOne({
+        where: {
+            id: order.id
+        },
+        include: Product
+    })
+    res.send(orderDetails);
   } catch (err) {
     next(err);
   }
